3350162a19897c0b2d397cf9de9f03db7ebb1563
Anish358 <anishejawale@gmail.com>
2022-03-20 14:36:42 +0530

    exchange page

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -3,7 +3,11 @@ import React from "react";
 const Exchanges = () => {
   return (
     <div>
-      <h1>Exchanges</h1>
+      <h1>Sorry for Inconvenience</h1>
+      <h1>
+        Due to Upgrade in Coin Ranking API, to list exchange we have to consider
+        a premium plan.
+      </h1>
     </div>
   );
 };
